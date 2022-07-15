d44c84fc99e424f89c3500c59698e2a7bcf9c677
Jason Louv <jason@ultraculture.org>
2022-07-14 19:56:46 -0500

    To 6

---
+++ Exercise.swift
@@ -0,0 +1,19 @@
+struct Exercise {
+    var name: String
+    var muscleGroups: [String]
+    var reps: Int
+    var sets: Int
+    var totalReps: Int
+
+    init(name: String, muscleGroups: [String], reps: Int, sets: Int) {
+        self.name = name
+        self.muscleGroups = muscleGroups
+        self.reps = reps
+        self.sets = sets
+        self.totalReps = reps * sets
+    }
+}
+
+var pushUp = Exercise(name: "Push up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
+
+//print(pushUp)
